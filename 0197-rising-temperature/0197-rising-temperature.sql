e6d62a2d68aa180f25acd11976e1111b74eca596
ellaliyy <147451024+ellaliyy@users.noreply.github.com>
2023-10-26 09:15:47 -0400

    Time: 782 ms (74.84%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -2,3 +2,5 @@
 SELECT t.id
 FROM Weather as t, Weather as y
 WHERE DATEDIFF(t.recordDate, y.recordDate) = 1 AND t.temperature > y.temperature
+
+-- If t.recordDate is one day later than y.recordDate, then the expression will evaluate to true.
