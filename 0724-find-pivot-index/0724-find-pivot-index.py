29846503ed0a189223de8b61a43111de2dc88ff5
ellaliyy <147451024+ellaliyy@users.noreply.github.com>
2023-11-01 11:07:37 -0400

    Time: 114 ms (37.65%), Space: 14.3 MB (73.62%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l_sum = 0
+        r_sum = sum(nums)
+        
+        for i, num in enumerate(nums):
+            r_sum -= num
+            
+            if l_sum == r_sum:
+                return i
+            
+            l_sum += num
+        
+        return -1
